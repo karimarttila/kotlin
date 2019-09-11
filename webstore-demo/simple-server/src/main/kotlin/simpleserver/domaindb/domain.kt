0e67609778f0b7e54784bec12ce15e1387590547
Kari Marttila <kari.marttila@iki.fi>
2019-09-11 20:40:58 +0300

    First Kotlin file and test skeleton

---
+++ webstore-demo/simple-server/src/main/kotlin/simpleserver/domaindb/domain.kt
@@ -0,0 +1,6 @@
+package simpleserver.domaindb
+
+fun getInfo(): String {
+    return "TODO"
+}
+
---
+++ webstore-demo/simple-server/src/test/kotlin/simpleserver/domaindb/domaintest.kt
@@ -0,0 +1,11 @@
+package simpleserver.domaindb
+
+import kotlin.test.assertEquals
+import org.junit.Test
+
+
+class TestSource {
+    @Test fun getInfoTest() {
+        assertEquals("TODO", getInfo())
+    }
+}
