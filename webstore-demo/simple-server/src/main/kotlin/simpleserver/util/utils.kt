c819759f418d258e70696a88f738c0ce950985ca
Kari Marttila <kari.marttila@iki.fi>
2019-10-13 17:57:50 +0300

    Added proxy object for csv data

--- webstore-demo/simple-server/src/main/kotlin/simpleserver/util/utils.kt
+++ webstore-demo/simple-server/src/main/kotlin/simpleserver/util/utils.kt
@@ -5,6 +5,7 @@ import com.opencsv.CSVReaderBuilder
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import java.io.File
+import java.util.concurrent.ConcurrentHashMap
 
 const val packageName = "util"
 val logger: Logger = LoggerFactory.getLogger(packageName)
@@ -13,14 +14,42 @@ sealed class CsvData
 data class CsvDataFound(val data: List<Array<String>>) : CsvData()
 object CsvDataNotFound : CsvData()
 
+/** Proxy for performance reasons. */
+val csvFiles = ConcurrentHashMap<String, CsvData>()
 
+/**
+ * Reads csv.
+ * If data found in proxy, returns that data.
+ * If data not found in proxy, reads data from file, adds data to proxy and returns data.
+ *
+ * @param fileName Filename to read (and used as key in proxy).
+ * @return CsvData entity
+ */
 public fun readCsv(fileName: String): CsvData {
     logger.debug(L_ENTER)
-    val url = ClassLoader.getSystemClassLoader().getResource(fileName) ?: return CsvDataNotFound
-    val filePath = File(url.file).toPath().toString()
-    val parser = CSVParserBuilder().withSeparator('\t').build()
-    val csvReader = CSVReaderBuilder(File(filePath).inputStream().bufferedReader()).withCSVParser(parser).build()
-    val ret = csvReader.use { input -> input.readAll() }
+    val csvData = csvFiles.get(fileName)
+    val ret = when (csvData) {
+        is CsvDataNotFound -> csvData
+        is CsvDataFound -> csvData
+        // Didn't find it in the proxy, let's read it from file.
+        null -> {
+            val url = ClassLoader.getSystemClassLoader().getResource(fileName)
+            val fromFile = when (url) {
+                null -> CsvDataNotFound
+                else -> {
+                    val filePath = File(url.file).toPath().toString()
+                    val parser = CSVParserBuilder().withSeparator('\t').build()
+                    val csvReader = CSVReaderBuilder(File(filePath).inputStream().bufferedReader()).withCSVParser(parser).build()
+                    CsvDataFound(csvReader.use { input -> input.readAll() })
+                }
+
+            }
+            // This filename read, add to proxy.
+            csvFiles.put(fileName, fromFile)
+            fromFile
+        }
+    }
     logger.debug(L_EXIT)
-    return CsvDataFound(ret)
+    return ret
 }
+
